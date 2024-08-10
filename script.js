8a7297b9bd060bdc47d6d46ab48081668be9806f
Parinam <parinamkcr7@gmail.com>
2024-08-10 12:01:26 +0545

    Undeleted, practise version

--- script.js
+++ script.js
@@ -123,25 +123,93 @@ const tnameObj  = document.getElementById('tname');
 
 const url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=Arsenal%5FF.C."
 
-fetch(url)
-  .then(function(response) {
-    const response1 = response.json();
-    return response1;
-  })
-  .then(function(response){
-    // html_code = response["parse"]["text"]["*"];
-    // parser = new DOMParser();
-    // html = parser.parseFromString(html_code, "text/html");
-    // var tables = html.querySelectorAll(".wikitable");
-    console.log(response);
-  })
-// async function getData() {
-// const requestURL = 'https://en.wikipedia.org/wiki/Arsenal_F.C.';
-// const request = new Request(requestURL);
-// const response = await fetch(request);
-// const receivedData = await response.json();
+const recentAddedPara = document.getElementById('recentAdd');
 
-// console.log(receivedData);
-// }
+// const url = "https://en.wikipedia.org/w/api.php?action=parse&formatversion=2&origin=*&prop=text&page=Arsenal%5FF.C.";
 
-// getData();
+
+// const abc = async fetch(url, {method: 'GET'})
+//   .then(function(response) {
+//     const response1 = response.json();
+//     return response1;
+//   })
+//   .then(function(response){
+//     // console.log(JSON.stringify(response, null, 2));
+
+//     dataContainer = response["parse"]["text"]["*"];
+//     // console.log(dataContainer);
+//     parser = new DOMParser();
+//     html1 = parser.parseFromString(dataContainer, "text/html");
+//     // var tables = html1.querySelectorAll("wikitable football-squad");
+//     td1 = html1.getElementsByTagName("table");
+//     return td1;
+//     // td2 = td1.getElementsByClassName("nogrid");
+//     // console.log(td1);
+//     // console.log(tables[0]);
+//   })
+
+// console.log(abc);
+
+const abc = fetch(url)
+            .then(function(response){
+                return response.json()})
+            .then(function(response){
+                const textFile = response["parse"]["text"]["*"];
+                // console.log(textFile);
+                const parser = new DOMParser();
+                //parsing response["parse"]["text"]["*"] into html object
+                const html1 = parser.parseFromString(textFile, "text/html");
+                // console.log(html1);
+                //finding all the tables inside the html1
+                const tableFinder = html1.getElementsByClassName("wikitable football-squad");
+                const thCollector = tableFinder[0].querySelectorAll('th');
+                console.log(thCollector);
+                // console.log(tableFinder);
+                // const finderByClassName = tableFinder[0].childNodes;
+                // console.log(finderByClassName);
+                // const tablesWithSquadClass = [];
+                var counter = 0;
+                [...tableFinder].forEach((tableFinderItem)=>{
+                    for(const node of tableFinderItem.children){
+                        var hasTr = node.querySelector("tr") != null;
+                        if(hasTr){
+                            const tdGrabber = node.getElementsByTagName("td");
+                            // console.log(tdGrabber);
+                            for (var i=0; i<tdGrabber.length; i++){
+                                // console.log(`${tdGrabber[i].innerText}`);
+                            }
+                        }
+                        // console.log(trGrabber);
+                        counter++;
+                        // console.log(trGrabber);
+                    //     // if(tdGrabber.length>0){
+                    //     //     console.log(tdGrabber);
+                        }
+                    //     console.log(tdGrabber);
+                    // }
+
+                    // const html2 = parser.parseFromString(textFile, "text/html");
+                    // console.log(html2);
+                    // console.log(tableFinderItem.childNodes);
+
+                    // const myTableBody = tableFinderItem.getElementsByTagName("tbody")[0];
+                    // console.log(myTableBody.innerHTML);
+                    // const allTr = tableFinderItem.querySelectorAll("td");
+                    // [...allTr].forEach((item) => console.log(item.innerText));
+                    // console.log(allTr);
+                    // // const newParser  = parser.parseFromString(tableFinderItem, "text/html");
+                    // // const checkClassNameInItem = newParser.getElementsByClassName("squad");
+                    // const checkClassNameInItem = tableFinderItem.getElementsByClassName("fn");
+                    // counter ++;
+                    // // const newParser2 = parser.parseFromString(checkClassNameInItem, "text/HTML");
+                    // // console.log(counter,newParser2);
+                    // console.log(counter,tableFinderItem.outerHTML);
+                    // // console.log(counter,tableFinderItem);
+                    // console.log(checkClassNameInItem.outerHTML);
+                    // console.log(tableFinderItem.innerHTML);
+
+                    // // console.log(newParser);
+                    // // console.log(counter, checkClassNameInItem);
+
+                })
+            });
